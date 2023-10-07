20d5de83a086c0bf400ea2a4b5a82d1658193de7
= <=>
2023-10-07 08:58:21 +0800

    Initial socket code for connection to the client with a password

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 const sockets = require('socket.io')
+const express = require('express')
+const http = require('http')
 
 const PASSWORD = '123456';
 
-const server = new sockets.Server({
-    port: 6161
-})
+app = express();
+
+const server = new sockets.Server(http.createServer(app), {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
 
 server.on('connection', socket => {
     console.log('New connection')
     socket.on('password', password => {
+        console.log('Password received: ', password)
         if (password === PASSWORD) {
             socket.emit('login', true)
         } else {
             socket.emit('login', false)
         }
-    }
-}
+    })
+});
+
+console.log('Server starting on port 3000');
+server.listen(3000);
